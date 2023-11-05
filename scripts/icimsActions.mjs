0bd6c9c3cd1329e6084179a8303608eacf0aac6c
doctorwait <prosnites@yandex.ru>
2023-11-05 16:17:05 +0400

    Icims basics are done

--- scripts/icimsActions.mjs
+++ scripts/icimsActions.mjs
@@ -17,13 +17,25 @@ icimsBtn.addEventListener('click', () => {
 });
 
 const fillForms = function () {
+  function value(selector, value) {
+    document.getElementById(selector).value = value;
+  }
+  function click(selector) {
+    document.getElementById(selector).click();
+  }
+  function dropdown(selector, ID) {
+    const list = document.getElementById(selector);
+    list.value = ID;
+    const event = new Event('change', { bubbles: true });
+    list.dispatchEvent(event);
+  }
+
   chrome.runtime.sendMessage(
     { contentScriptQuery: 'loadJSON' },
-    function (data) {
-      const runEmail = function (data) {
-        document.getElementById('email').value = data.email;
-        document.getElementById('enterEmailSubmitButton').click();
-      };
+    async function (data) {
+      async function wait(sec = 1) {
+        return new Promise(resolve => setTimeout(resolve, sec * 1000));
+      }
 
       /*
       const runLoadResume = function (data) {
@@ -33,28 +45,44 @@ const fillForms = function () {
           function (resume) {
             const input = document.getElementById(
               'PortalProfileFields.Resume_File'
-            );
-            const dt = new DataTransfer();
-            dt.items.add(resume);
-            input.files = dt.files;
+              );
+              const dt = new DataTransfer();
+              dt.items.add(resume);
+              input.files = dt.files;
             const event = new Event('change', {
               bubbles: !0,
             });
             input.dispatchEvent(event);
           }
-        );
-      };
-      **/
-
-      const runMainQuestions = function (data) {
-        
-      }
-
-      const runs = [runEmail, runMainQuestions];
+          );
+        };
+        **/
+      const runs = [
+        [value, 'email', data.email],
+        [click, 'enterEmailSubmitButton'],
+        [value, 'PersonProfileFields.Login', data.login],
+        [value, 'PersonProfileFields.Password', data.password],
+        [value, 'PersonProfileFields.Password_Confirm', data.password],
+        [value, 'PersonProfileFields.FirstName', data.firstName],
+        [value, 'PersonProfileFields.LastName', data.lastName],
+        [dropdown, '-1_PersonProfileFields.PhoneType', '15757'], // TODO: Сделать выбор для пользователя
+        [value, '-1_PersonProfileFields.PhoneNumber', data.phoneNumber],
+        [dropdown, '-1_PersonProfileFields.AddressType', '15763'],
+        [value, '-1_PersonProfileFields.AddressStreet1', data.street],
+        [value, '-1_PersonProfileFields.AddressCity', data.city],
+        [value, '-1_PersonProfileFields.AddressZip', data.postalCode],
+        [
+          dropdown,
+          '-1_PersonProfileFields.AddressCountry_icimsDropdown',
+          '12781',
+        ],
+        [dropdown, 'rcf3048', data.vacancySource],
+        [dropdown, 'rcf3049', data.vacancySourceSpecified],
+      ];
 
       for (const run of runs) {
         try {
-          run(data);
+          run[0](...run.slice(1));
         } catch (error) {
           console.error('Ошибка в функции: ' + error);
         }
